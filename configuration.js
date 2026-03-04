4a6d68e9a32bbc5a40c31dcfb2362199ae559b39
davbotapp <davbotapp@gmail.com>
2026-03-04 11:19:22 +0100

    Create configuration.js

---
+++ configuration.js
@@ -0,0 +1,26 @@
+// Configurations centrales pour tous les jetons et fichiers
+window.PRODUCTS_CONFIG = {
+  tokens: {
+    "N3": { name: "Jeton N3", duration: 3, files: [
+      { title: "Howdy VPN 1", link: "howdy://file1", detail: "Fiche Howdy VPN 3 jours" },
+      { title: "Howdy VPN 2", link: "howdy://file2", detail: "Fiche Howdy VPN 3 jours" },
+      { title: "Howdy VPN 3", link: "howdy://file3", detail: "Fiche Howdy VPN 3 jours" },
+      { title: "Howdy VPN 4", link: "howdy://file4", detail: "Fiche Howdy VPN 3 jours" },
+      { title: "Howdy VPN 5", link: "howdy://file5", detail: "Fiche Howdy VPN 3 jours" }
+    ]},
+    "P7": { name: "Jeton P7", duration: 7, files: [
+      { title: "Maya VPN 1", link: "maya://file1", detail: "Fiche Maya VPN 7 jours" },
+      { title: "Maya VPN 2", link: "maya://file2", detail: "Fiche Maya VPN 7 jours" },
+      { title: "Maya VPN 3", link: "maya://file3", detail: "Fiche Maya VPN 7 jours" },
+      { title: "Maya VPN 4", link: "maya://file4", detail: "Fiche Maya VPN 7 jours" },
+      { title: "Maya VPN 5", link: "maya://file5", detail: "Fiche Maya VPN 7 jours" }
+    ]},
+    "M30": { name: "Jeton M30", duration: 30, files: [
+      { title: "Wryvpn ✨", link: "maya://m30file1", detail: "Fiche Maya VPN 30 jours" },
+      { title: "Maya VPN 2", link: "maya://m30file2", detail: "Fiche Maya VPN 30 jours" },
+      { title: "Maya VPN 3", link: "maya://m30file3", detail: "Fiche Maya VPN 30 jours" },
+      { title: "Maya VPN 4", link: "maya://m30file4", detail: "Fiche Maya VPN 30 jours" },
+      { title: "Maya VPN 5", link: "maya://m30file5", detail: "Fiche Maya VPN 30 jours" }
+    ]}
+  }
+};
